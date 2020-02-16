54f778fc6ce085f7b295822b454f61f45e198de2
Arun Arjunan <arun.arjunan1@outlook.com>
2020-02-16 14:41:01 +0800

    updates

--- RLE_ALGS/README.md
+++ RLE_ALGS/README.md
@@ -1,1 +1,7 @@
-#Computer Science Projects
+#Run Length Encoding Algs
+
+RLEC is used to compress any binary or alphanumeric string
+
+RLED is used to decompress any COMPRESSED binary or alphanumeric string back into raw form.
+
+ASCII_ALPHANUMERIC_CONVERSION is used to convert alphanumeric inputs into ascii, for compression.
--- RLE_ALGS/RLE.py
+++ RLE_ALGS/RLE.py
@@ -1,10 +1,6 @@
 # RLE encoder and decoder
 class RLE():
 
-    # these algs are for compressing and decompressing binary data.
-    # they're also able to process alphanumeric strings, without binary conversion.
-    # ASCII_CONVERSION is able to convert alphanumeric strings into ascii and ba
-
     def RLEC(self, inputstream):
 
         iter_char = ''

c1954e199c42dc1d37dc43f59c4b61a87a325406
Arun Arjunan <arun.arjunan1@outlook.com>
2020-02-18 09:23:28 +0800

    updates

--- RLE_ALGS/main.py
+++ RLE_ALGS/main.py
@@ -10,7 +10,12 @@ decode_string = decoding.read()
 # decoding.write(RLE.RLEC(1, encode_string))
 
 
-# print(RLE.RLEC(1, encode_string))
-print(RLE.RLED(1, decode_string))
+print(RLE.RLEC(1, encode_string))
+# print(RLE.RLED(1, decode_string))
 
 # RLE.RLE_ASCII(1, encode_string)
+
+
+
+
+print(1- (len(decode_string) / len(encode_string)))

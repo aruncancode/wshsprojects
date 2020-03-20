59e096d39c5d6be02fb3ef39f287414e61b32fa9
Arun Arjunan <arun.arjunan1@outlook.com>
2020-03-20 20:40:56 +0800

    updates

--- 005_Colour_Mixer/main.py
+++ 005_Colour_Mixer/main.py
@@ -4,13 +4,13 @@ master = Tk()
 
 
 def test(decimal):
-    red = r.get()
-    green = g.get()
-    blue = b.get()
+    # red = r.get()
+    # green = g.get()
+    # blue = b.get()
 
-    red = hex(red).split('x')[-1]
-    green = hex(green).split('x')[-1]
-    blue = hex(blue).split('x')[-1]
+    red = hex(r.get()).split('x')[-1]
+    green = hex(g.get()).split('x')[-1]
+    blue = hex(b.get()).split('x')[-1]
 
     if len(red) == 1:
         red = '0' + red
@@ -19,21 +19,25 @@ def test(decimal):
     if len(blue) == 1:
         blue = '0' + blue
 
-    # print(red, green, blue)
-
     mix = '#' + str(red) + str(green) + str(blue)
     mixed_label['bg'] = mix
+    hex_label['text'] = mix
+    hex_label['fg'] = mix
 
+
+hex_label = Label(text='#ffffff', fg='black', font=('Consolas', 25))
+hex_label.grid(column=1)
 
 mixed_label = Label(bg='#ffffff')
-mixed_label.grid(row=1, column=3, ipadx=100, ipady=100)
+mixed_label.grid(column=1, ipadx=100, ipady=100)
 
 r = Scale(master, from_=0, to=255, orient=HORIZONTAL, command=test, fg='red')
-r.grid(row=0, column=1)
+r.grid(column=1)
 g = Scale(master, from_=0, to=255, orient=HORIZONTAL, command=test, fg='green')
-g.grid(row=1, column=1)
+g.grid(column=1)
 b = Scale(master, from_=0, to=255, orient=HORIZONTAL, command=test, fg='blue')
-b.grid(row=2, column=1)
+b.grid(column=1)
 
 
+master.title('Colour Mixer')
 master.mainloop()

e6cf67ddacf129d9f052c42eeaee8091954c3399
Arun Arjunan <arun.arjunan1@outlook.com>
2020-03-24 22:43:10 +0800

    added class

--- 006_Caesar_Cipher/main.py
+++ 006_Caesar_Cipher/main.py
@@ -1,39 +1,42 @@
-def encrypt(text, s):
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    shitfted_alphabet = alphabet[s:] + alphabet[:s]
-    output = ""
+class CaesarCipher():
 
-    for e in text:
-        if e.isupper():
-            index = alphabet.index(e)
-            output += shitfted_alphabet[index]
-        elif e.islower():
-            index = alphabet.index(e.upper())
-            output += (shitfted_alphabet[index]).lower()
-        else:
-            output += e
-    return output
+    def encrypt(self, text, s):
+        alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        shitfted_alphabet = alphabet[s:] + alphabet[:s]
+        output = ""
+
+        for e in text:
+            if e.isupper():
+                index = alphabet.index(e)
+                output += shitfted_alphabet[index]
+            elif e.islower():
+                index = alphabet.index(e.upper())
+                output += (shitfted_alphabet[index]).lower()
+            else:
+                output += e
+        return output
+
+    def decrypt(self, text, s):
+        alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        shitfted_alphabet = alphabet[s:] + alphabet[:s]
+        output = ""
+
+        for e in text:
+            if e.isupper():
+                index = shitfted_alphabet.index(e)
+                output += shitfted_alphabet[index]
+            elif e.islower():
+                e = e.upper()
+                index = shitfted_alphabet.index(e)
+                output += (alphabet[index]).lower()
+            else:
+                output += e
+        return output
 
 
-def decrypt(text, s):
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    shitfted_alphabet = alphabet[s:] + alphabet[:s]
-    output = ""
+C = CaesarCipher()
 
-    for e in text:
-        if e.isupper():
-            index = shitfted_alphabet.index(e)
-            output += shitfted_alphabet[index]
-        elif e.islower():
-            e = e.upper()
-            index = shitfted_alphabet.index(e)
-            output += (alphabet[index]).lower()
-        else:
-            output += e
-    return output
-
-
-print("ENCRYPTED", encrypt("ArunARUN", -2))
-print("DECRYPTED", decrypt("zqtmozrrvnqcsdrs123234", 25))
+print("ENCRYPTED", C.encrypt("ozno", 5))
+print("DECRYPTED", C.decrypt("fyyfhp fy ifbs", -21))

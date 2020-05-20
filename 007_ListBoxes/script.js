2ff2c4e64882a1bdf7f14f7693025690caefeaaf
Arun Arjunan <arun.arjunan1@outlook.com>
2020-05-21 00:39:42 +0800

    added second box, and expanded gui. css needs work

--- 007_ListBoxes/script.js
+++ 007_ListBoxes/script.js
@@ -1,3 +1,8 @@
+var x = {
+    "one": [1, 2, 3, 4],
+    "two": [2]
+};
+
 function addValue() {
     var add_value = document.getElementById("addBox").value;
     if (add_value == "") {
@@ -7,8 +12,37 @@ function addValue() {
         var opt = document.createElement("option");
         opt.innerHTML = add_value;
         opt.value = add_value;
+        opt.onclick = function () { showValue(); };
+        x[add_value] = [];
         document.getElementById("options").add(opt);
         document.getElementById("addBox").value = "";
+
+    }
+}
+
+function addValue2() {
+    var add_value = document.getElementById("addBox").value;
+    var selectTrue = document.getElementById("options").selectedIndex;
+    if (add_value == "") {
+        alert("please enter a value");
+    }
+    else {
+
+        if (selectTrue == -1) {
+            alert("please select a value from box1");
+        }
+
+        else {
+
+            var selectValue = document.getElementById("options").value;
+            var opt = document.createElement("option");
+            opt.innerHTML = add_value;
+            opt.value = selectValue;
+            document.getElementById("options2").add(opt);
+            x[selectValue].push(add_value);
+            document.getElementById("addBox").value = "";
+
+        }
     }
 }
 
@@ -18,6 +52,31 @@ function delValue() {
         alert("please select an option to delete");
     }
     else {
+        delete x[option_menu.value];
         option_menu.remove(option_menu.selectedIndex);
+    }
+}
+
+function delValue2() {
+    var option_menu = document.getElementById("options2");
+    if (option_menu.selectedIndex == -1) {
+        alert("please select an option to delete");
+    }
+    else {
+        x[option_menu.value].splice(option_menu.selectedIndex, 1);
+        option_menu.remove(option_menu.selectedIndex);
+    }
+}
+
+
+function showValue() {
+    document.getElementById("options2").options.length = 0;
+    var selected_value = document.getElementById("options").value;
+
+    for (var i in x[selected_value]) {
+        var new_opt = document.createElement("option");
+        new_opt.innerHTML = x[selected_value][i];
+        new_opt.value = selected_value;
+        document.getElementById("options2").add(new_opt);
     }
 }
